3cd81940e265f27b3cb0f67b743975dd7914f13f
Mohsin Khan <mohsinkhantwy@gmail.com>
2025-09-08 03:26:22 -0700

    js assignment 1

---
+++ js assignment 1/app.js
@@ -0,0 +1,480 @@
+//                                 Chapter Number 1
+
+// 1) Question number 1: Alerts
+// alert("Hello, World!");
+
+
+// 2) Question number 2: Alerts 
+// alert("Error! Please enter a valid password.");
+
+
+// 3) Question number 3: Alerts
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+
+// 4) Question number 4: Alerts
+// alert("How are you?");
+// alert("I'm fine, thank you!");
+
+
+// 5) Question number 5: Alerts
+// alert("Hello,,, I can run JS through my web browser's console.");
+
+
+// 6) Question number 6: Alerts
+// Done
+
+
+// 7) Question number 7: Alerts
+// Done
+
+
+//                                                   Chapter Number 2
+
+
+// 8) Question number 8: variable for strings
+// var username;
+
+
+// 9) Question number 9: variable for strings
+// var myName = "Mohsin Khan";
+
+
+// 10) Question number 10: variable for strings
+// var message = "Hello, World!";
+// alert(message);
+
+
+// 11) Question number 11: variable for strings
+// var name = "Mohsin khan";
+// alert(name);
+// var age = "19 Years Old";
+// alert(age);
+// var qualification = "Certified Mobile Application Development";
+// alert(qualification);
+
+
+// 12) Question number 12: variable for strings
+// var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizza);
+
+
+// 13) Question number 13: variable for strings
+// var email = "mohsin124@gmail.com";
+// alert("My email address is " + email);
+
+
+// 14) Question number 14: variable for strings
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+
+// 15) Question number 15: variable for strings
+// var message = "Yah! I can write HTML content through JavaScript";
+// document.write(message);
+
+
+// 16) Question number 16: variable for strings
+//  var intro = "My name is Mohsin Khan";
+//  alert(intro);
+//  document.write(intro);
+
+
+//                                        Chapter Number 3     
+
+
+// 17) Question number 17: variable for number
+// var age = 19;
+// alert("I am " + age + " years old.");
+
+
+// 18) Question number 18: variable for number
+// var visits = 14;
+// alert("You have visited this site " + visits + " time(s).");
+
+
+// 19) Question number 19: variable for number
+// var birth = 2006;
+// document.write("My birth year is " + birth + "<br> Data type of my declared variable is " + typeof(birth));
+
+
+// 20) Question number 20: variable for number
+// var visitorName = "John Doe";
+// var title = "5 T-shirts";
+// document.write(visitorName + " ordered " + title +  " on XYZ Clothing store");
+
+
+
+//                             Chapter Number 4 
+
+
+
+// 21) Question number 21: LEGAL & ILLEGAL
+// var name = "Mohsin khan";
+// var age = 19;
+// var city = "Karachi";
+
+
+// 22) Question number 22: LEGAL & ILLEGAL
+// var name = "mohsin";
+// var lastname = "khan";
+// var age = 19;
+// var city = "karachi";
+// var birth = 2006;
+
+
+// var my-name; 
+// var user name; 
+// var @city;
+// var 1name;  
+// var case;
+
+
+// 23) Question number 23: LEGAL & ILLEGAL
+
+// document.writeln("Ruls for naming JS variables\n\n\n\n");
+
+// var numbers = "numbers, $";
+// var num = 5;
+
+// document.write("Variable name can only contain " + numbers + "and " + num + ". For example : $my_1stVariable");
+
+// var letter = "Letter, $";
+// var mai = "_";
+// document.write("Variable must begin with a " + letter + " or" + mai + ". For example : $name, _name or name");
+
+// var sen = "sensitive";
+// document.write("Variable names are case " + sen);
+
+
+// var keyword = "Keywords";
+// document.write("Variable names should not be JS " + keyword);
+
+
+
+//                                    Chapter Number 5
+
+
+// 24) Question number 24: Math Expression
+// var num = 3;
+// var num2 = 5;
+// var res = num + num2;
+// document.write("Sum of " + num + " and " + num2 + " is " + res);
+
+
+
+// 25) Question number 25: Math Expression
+
+    // var num1 = +prompt("Enter first number:");
+    // var num2 = +prompt("Enter second number:");
+
+    // // Subtraction
+    // var subResult = num1 - num2;
+    // document.write("Subtraction: " + subResult + "<br><br>");
+
+    // // Multiplication
+    // var multiResult = num1 * num2;
+    // document.write("Multiplication: " + multiResult + "<br><br>");
+
+    // // Division
+    // var dviResult = num1 / num2;
+    // document.write("Division: " + dviResult + "<br><br>");
+
+    // // Modulus (remainder)
+    // var modulResult = num1 % num2;
+    // document.write("Modulus: " + modulResult + "<br><br>");
+
+
+
+    // 26) Question number 26: Math Expression
+
+    // a)
+    //  var value;
+
+
+    // //  b)
+    // document.write("Value after variable declaration is: " + value + "<br>");
+
+    // // c)
+    // var myValue = 5;
+
+
+    // // d)
+    // document.write("Initial value: " + myValue + "<br>");
+
+    // // e)
+    // myValue++;
+
+    // // f)
+    // document.write("Value after increment is: " + myValue + "<br>");
+
+    // // g)
+    // var myVal = myValue + 7;
+
+    // // h)
+    // document.write("Value after addition is: " + myVal + "<br>");
+
+    // // i)
+    // myVal--;
+
+    // // j)
+    // document.write("Value after decrement is: " + myVal + "<br>");
+
+
+    // // k)
+    // ValModule = myVal % 3;
+
+    // // l)
+    // document.write("The remainder is: " + ValModule + "<br>");
+
+
+
+    // 27) Question number 27: Math Expression
+
+    // var cost = 600;
+    // var ticket = 5;
+
+    // document.write("Total cost to buy " + ticket + " Tickets to a movie is: " + cost * ticket + " PKR <br>");
+
+
+
+// 28) Question number 28: Math Expression
+//  var num = 4; 
+
+//     document.write("Table of " + num + "<br><br>");
+
+//     var multi1 = num * 1;
+//     var multi2 = num * 2;
+//     var multi3 = num * 3;
+//     var multi4 = num * 4;
+//     var multi5 = num * 5;
+//     var multi6 = num * 6;
+//     var multi7 = num * 7;
+//     var multi8 = num * 8;
+//     var multi9 = num * 9;
+//     var multi10 = num * 10;
+
+//     document.write(num + " x 1 = " + multi1 + "<br>");
+//     document.write(num + " x 2 = " + multi2 + "<br>");
+//     document.write(num + " x 3 = " + multi3 + "<br>");
+//     document.write(num + " x 4 = " + multi4 + "<br>");
+//     document.write(num + " x 5 = " + multi5 + "<br>");
+//     document.write(num + " x 6 = " + multi6 + "<br>");
+//     document.write(num + " x 7 = " + multi7 + "<br>");
+//     document.write(num + " x 8 = " + multi8 + "<br>");
+//     document.write(num + " x 9 = " + multi9 + "<br>");
+//     document.write(num + " x 10 = " + multi10  + "<br>");
+
+
+
+// 29) Question number 29: Math Expression
+
+// a. Store Celsius temperature
+// var celsius = 25;
+
+// b. Convert Celsius to Fahrenheit
+// var fahrenheit = (celsius * 9/5) + 32;
+// document.write(celsius + "°C is " + fahrenheit + "°F <br>");
+
+// c. Store Fahrenheit temperature
+// var fahrenheit2 = 70;
+
+// d. Convert Fahrenheit to Celsius
+// var celsius2 = (fahrenheit2 - 32) * 5/9;
+// document.write(fahrenheit2 + "°F is " + celsius2 + "°C");
+
+
+
+// 30) Question number 30: Math Expression
+
+// var priceItem1 = 650;
+// var priceItem2 = 100;
+// var qtyItem1 = 3;
+// var qtyItem2 = 7;
+// var shippingCharges = 100;
+
+// var totalcast = priceItem1 + priceItem2 + qtyItem1 + qtyItem2 + shippingCharges;
+
+// document.write("<h2>Shopping Cart</h2>");
+// document.write("Price of item 1 is " + priceItem1 + "<br>");
+// document.write("Quantity of item 1 is " + qtyItem1 + "<br>");
+// document.write("Price of item 2 is " + priceItem2 + "<br>");
+// document.write("Quantity of item 2 is " + qtyItem2 + "<br>");
+// document.write("Shipping Charges: " + shippingCharges + "<br><br>");
+// document.write("Total cost of your order is " + totalCost);
+
+
+
+// 31) Question number 31: Math Expression
+
+// var eng  = 490;
+// var urdu = 300;
+// var res = eng + urdu;
+// var total = 900;
+// var per = (res / total) * 100;
+
+
+// document.write("<h1> Marks Sheet </h1><br>");
+// document.write("Total Marks " + total +"<br>");
+// document.write("Marks Obtained " + res +"<br>");
+// document.write("Percentage " + per +"<br>");
+
+
+
+// 32) Question number 32: Math Expression  
+
+
+// document.write("<h1> Currency  in PKR </h1><br>");
+
+// var doller = 10;
+// var riyal = 25;
+
+// var convert = doller * 104.80;
+// var convert_pk = riyal * 28;
+
+// document.write("Total Currency in PKR: ", convert + convert_pk);
+
+
+
+
+// 33) Question number 33: Math Expression 
+
+// var a = 5;
+// var b = 10;
+// var c = 2;
+
+// var res = (a + 5) * b / c;
+
+// document.write(res);
+
+
+
+// 34) Question number 34: Math Expression 
+
+
+// var year = 2025;
+// var birth = 2007;
+// var age = 18;
+
+// document.write("<h1> Age Calculater </h1><br>");
+// document.write("Current Year: " + year + "<br>");
+// document.write("Birth Year: " + birth + "<br>");
+// document.write("Your Age is : " + age + "<br>");
+
+
+
+// 35) Question number 35: Math Expression 
+
+
+// Samaj nahi araha he
+
+
+
+
+// 36)  Question number 36: Math Expression 
+
+// var snake = "chocolate chip";
+// var age = 19;
+// var max_age = "I Don't Know";
+// var daily = 3;
+
+
+// document.write("<h1> The Lifetime Supply Calculater</h1><br>");
+// document.write("Favourite Snake: " + snake + "<br>");
+// document.write("Current Age: " + age + "<br>");
+// document.write("Estimated Maximum Age: " + max_age + "<br>");
+// document.write("Amount of snakes per day: " + daily + "<br>");
+// document.write("You will need 150 chocolate chip to last you until the ripe old age of 65");
+
+
+
+
+//                                     Chapter Number 6 - 9
+
+
+
+
+// 37) Question number 37: Math Expression
+
+// document.write("Results");
+
+// var a = 10;
+// document.write("The value of a is: " + a + "<br><br><br>");
+
+// document.write("The value of ++a is: 11 <br>");
+// document.write("The value of a is: " + ++a + "<br><br><br>");
+
+// document.write("The value of ++a is: 11 <br>");
+// document.write("The value of a is: " + ++a + "<br><br><br>");
+
+// document.write("The value of --a is: 11 <br>");
+// document.write("The value of a is: " + --a + "<br><br><br>");
+
+// document.write("The value of --a is: 11 <br>");
+// document.write("The value of a is: " + --a + "<br><br><br>");
+
+
+
+// 38) Question number 38: Math Expression
+
+// var a = 2;
+// var b = 1;
+
+// var Results = --a - --b + ++b + b--;
+
+// document.write("a is " + a + "<br>");
+// document.write("b is " + b + "<br>");
+// document.write("result is " + Results);
+
+
+
+// 39) Question number 39: Math Expression
+
+// prompt("what is Your name:");
+
+// var userName = "Mohsin";
+// alert("Welcome " + userName + "!");
+
+
+// 40) Question number 40: Math Expression
+
+//   var num = 5;
+//   prompt("Enter a number:", num);
+
+//     document.write("Multiplication Table of " + num + "<br><br>");
+//     document.write(num + " x 1 = " + (num * 1) + "<br>");
+//     document.write(num + " x 2 = " + (num * 2) + "<br>");
+//     document.write(num + " x 3 = " + (num * 3) + "<br>");
+//     document.write(num + " x 4 = " + (num * 4) + "<br>");
+//     document.write(num + " x 5 = " + (num * 5) + "<br>");
+//     document.write(num + " x 6 = " + (num * 6) + "<br>");
+//     document.write(num + " x 7 = " + (num * 7) + "<br>");
+//     document.write(num + " x 8 = " + (num * 8) + "<br>");
+//     document.write(num + " x 9 = " + (num * 9) + "<br>");
+//     document.write(num + " x 10 = " + (num * 10) + "<br>");
+
+
+
+// 41) Question number 41: Math Expression
+
+// document.write("Subject Total Marks Obtained Marks Percentage <br><br>");
+
+// var marks = 100;
+
+// var engMarks = 54;
+// var mathMarks = 54;
+// var urduMarks = 48;
+
+// var res = engMarks + mathMarks + urduMarks;
+
+
+// document.write("English " + marks + "&nbsp;&nbsp;&nbsp;&nbsp; " + engMarks + "&nbsp;&nbsp;&nbsp;&nbsp; " + engMarks +"%<br>");
+// document.write("Math&nbsp;&nbsp;&nbsp;&nbsp; " + marks + "&nbsp;&nbsp;&nbsp;&nbsp; " + mathMarks + "&nbsp;&nbsp;&nbsp;&nbsp; " + mathMarks +"%<br>");
+// document.write("Urdu&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + marks + "&nbsp;&nbsp;&nbsp;&nbsp; " + urduMarks + "&nbsp;&nbsp;&nbsp;&nbsp; " + urduMarks +"%");
+
+
+// document.write("<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;300 &nbsp;&nbsp;&nbsp;" + res + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;52%");
+
+
+
+//                                                      THE END ASSIGNMENT
